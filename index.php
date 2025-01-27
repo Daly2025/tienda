8c196f6fe08db1e806b8767b9dd9cc94e898ee95
Daly2025 <lydiafdez84@gmail.com>
2025-01-27 17:01:26 +0100

    ly

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>tienda>
 </head>
 <body>
     <form action="" method="post"></form>
